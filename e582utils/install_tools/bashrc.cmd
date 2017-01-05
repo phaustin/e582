23439d8ce7f77b10eb36902e185e23b1344d2887
Phil Austin <paustin@eos.ubc.ca>
2017-01-05 09:11:33 -0800

    typo

--- e582utils/install_tools/bashrc.cmd
+++ e582utils/install_tools/bashrc.cmd
@@ -1,6 +1,5 @@
-@echo off
 @echo "running bash.cmd"
 set HOME=%HOMEDRIVE%\%HOMEPATH%
 set PATH=c:\msys64\usr\bin;%PATH%
-set PYTHONPATH=%HOME%\pythonlibs;%HOME%\repos\e582_2017
+set PYTHONPATH=%HOME%\repos\pythonlibs
 set PWD=%~dp0
