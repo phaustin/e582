3a10e9df39d826b7742a6c55f18d1270716e7874
Phil Austin <you@example.comw>
2023-03-14 12:23:54 -0700

    fresh start

--- e582utils/install_tools/shell_rcfiles/bashrc.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-@echo "running bashrc.cmd"
-set HOME=%HOMEDRIVE%\%HOMEPATH%
-set PATH=c:\msys64\usr\bin;%PATH%
-set PYTHONPATH=%HOME%\repos\pythonlibs
-set PWD=%~dp0
--- e582utils/windows/scripts/killprocs.cmd
+++
@@ -1,8 +0,0 @@
-@rem -- call a long python module with a short cmd script
-@rem -- %* passes all arguments after the command name to the
-@rem -- python script
-@rem example:  killprocs mini3
-@rem will run  python -m e582utils.killprocs mini3
-@echo off
-python -m e582utils.killprocs %*
-
